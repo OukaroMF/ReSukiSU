d6b579593e6a1de8c084d0af87a1d713401686ab
YC酱luyancib <luyancib@qq.com>
2026-03-08 23:29:39 +0800

    sync susfs
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/4390fd220892dc9d60b80fcde604437382a7668f
    
    maybe this commit i pushed is not full. Because is different from upstream patch.

--- kernel/setuid_hook.c
+++ kernel/setuid_hook.c
@@ -29,6 +29,62 @@
 #include "kernel_umount.h"
 #include "sulog.h"
 
+#ifdef CONFIG_KSU_SUSFS
+static inline bool is_zygote_isolated_service_uid(uid_t uid)
+{
+    uid %= 100000;
+    return (uid >= 99000 && uid < 100000);
+}
+
+static inline bool is_zygote_normal_app_uid(uid_t uid)
+{
+    uid %= 100000;
+    return (uid >= 10000 && uid < 19999);
+}
+
+extern u32 susfs_zygote_sid;
+extern struct cred *ksu_cred;
+
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+extern void susfs_run_sus_path_loop(void);
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
+
+struct susfs_handle_setuid_tw {
+    struct callback_head cb;
+};
+
+static void susfs_handle_setuid_tw_func(struct callback_head *cb)
+{
+    struct susfs_handle_setuid_tw *tw = container_of(cb, struct susfs_handle_setuid_tw, cb);
+    const struct cred *saved = override_creds(ksu_cred);
+
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+    susfs_run_sus_path_loop();
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
+
+    revert_creds(saved);
+    kfree(tw);
+}
+
+static void ksu_handle_extra_susfs_work(void)
+{
+    struct susfs_handle_setuid_tw *tw = kzalloc(sizeof(*tw), GFP_ATOMIC);
+
+    if (!tw) {
+        pr_err("susfs: No enough memory\n");
+        return;
+  }
+
+    tw->cb.func = susfs_handle_setuid_tw_func;
+
+    int err = task_work_add(current, &tw->cb, TWA_RESUME);
+    if (err) {
+        kfree(tw);
+        pr_err("susfs: Failed adding task_work 'susfs_handle_setuid_tw', err: %d\n", err);
+    }
+}
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 extern void disable_seccomp(struct task_struct *tsk);
 
 static inline void ksu_set_file_immutable(const char *path_name, bool immutable)
@@ -177,7 +233,9 @@ int ksu_handle_setuid(uid_t new_uid, uid_t old_uid, uid_t euid) // (new_euid)
 
     // Handle kernel umount
     ksu_handle_umount(old_uid, new_uid);
-
+#ifdef CONFIG_KSU_SUSFS
+    ksu_handle_extra_susfs_work();
+#endif
     return 0;
 }
 
